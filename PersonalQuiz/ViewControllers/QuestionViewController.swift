539d5439ec11aaa5539680e86894ae459f99b152
DmKeller <dmit.keller@gmail.com>
2023-04-10 01:43:44 +0300

    fatal error questionLabel

--- PersonalQuiz/ViewControllers/QuestionViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionViewController.swift
@@ -7,13 +7,142 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController {
-
+final class QuestionViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet weak var rangedSlider: UISlider!
+    
+    // MARK: - Private properties
+    private var questionIndex = 0
+    private let questions = Question.getQuestions()
+    private var answersChosen: [Answer] = []
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        let answerCount = Float(currentAnswers.count - 1)
+        rangedSlider.maximumValue = answerCount
+        rangedSlider.value = answerCount / 2
     }
-
-
+    
+    // MARK: - Override Metods
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let resultVS = segue.destination as? ResultViewController else { return }
+        resultVS.answersChosen = answersChosen
+    }
+    // MARK: - IB Actions
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
+        let currentAnswer = currentAnswers[buttonIndex]
+        answersChosen.append(currentAnswer)
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnswerButtonPressed() {
+        for (multipleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
+            if multipleSwitch.isOn {
+                answersChosen.append(answer)
+            }
+        }
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        let index = lrintf(rangedSlider.value)
+        answersChosen.append(currentAnswers[index])
+        nextQuestion()
+    }
 }
+
+// MARK: - Private Methods
+private extension QuestionViewController {
+    func updateUI() {
+        // Hide everything
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        // Get current question
+        let currentQuestion = questions[questionIndex]
+        
+        // Set current question for question label
+        questionLabel.text = currentQuestion.title
+        
+        // Calculate progress
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        // Set progress for progress view
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        // Set navigation title
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        // Show stacks corresponding to question type
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    /// Show stack view with single answers
+    ///
+    /// Display a stack view of single answers category
+    ///
+    /// - Parameter type: Specifies the category of responses
+    func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single: showSingleStackView(with: currentAnswers)
+        case .multiple: showMultipleStackView(with: currentAnswers)
+        case .ranged: showRangedStackView(with: currentAnswers)
+        }
+    }
+    
+    func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden.toggle()
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+    
+    func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden.toggle()
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.title
+        }
+    }
+    
+    func showRangedStackView(with answers: [Answer]) {
+        rangedStackView.isHidden.toggle()
+        
+        rangedLabels.first?.text = answers.first?.title
+        rangedLabels.last?.text = answers.last?.title
+    }
+    
+    func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+            return
+        }
+        
+        performSegue(withIdentifier: "showResult", sender: nil)
+    }
+}
+
 
--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -7,14 +7,31 @@
 
 import UIKit
 
-class ResultViewController: UIViewController {
-
+final class ResultViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var emojiResultLabel: UILabel!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // MARK: - Private properties
+    var answersChosen: [Answer]!
+   
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+        navigationItem.hidesBackButton = true
+        // answerAnimalEmojiLabel.text = "Вы - \()"
     }
     
+    // MARK: - Private Methods
 
-
+    //    private func answerWhatAnimal() {
+    //        var answersChosenAnimal: [Animal : Int] = [:]
+    //        for answer in answersChosen {
+    //            if animal
+    //            answersChosenAnimal.append(answer : countAnswer)
+    //        }
+    //    }
 }

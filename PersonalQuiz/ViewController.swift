99273d140318fc75da1d7af60f3e9e4e5e6afd38
DmKeller <dmit.keller@gmail.com>
2023-04-09 23:52:40 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Келлер Дмитрий on 09.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
